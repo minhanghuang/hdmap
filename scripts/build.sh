2f3f0b8684a37eec42d7340a54f500b501a98986
minhanghuang <job@minhang.me>
2023-06-01 14:51:35 +0800

    ci: add ci script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ function main() {
   echo "--------------------- opendrive-engine build ------------------"
   echo "---------------------------------------------------------------"
   mkdir -p build && cd build
-  cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug ..
+  cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug -DBUILD_OPENDRIVE_ENGINE_TEST=ON ..
   make -j4
 }
 
